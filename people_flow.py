cd45c7fc48fcec55f491692efaa148c61ea68eb6
alex <cyy0523xc@gmail.com>
2019-04-26 21:02:41 +0800

    *

--- people_flow.py
+++ people_flow.py
@@ -242,6 +242,9 @@ def detect_video(yolo, video_path, output_path=0):
             curr_fps = 0
         cv2.putText(result, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                     fontScale=0.50, color=(255, 0, 0), thickness=2)
+        height, width = result.shape[:2]
+        cv2.putText(result, 'DeeAo AI Team', (width-80, height-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (128, 255, 0), 2)
         # cv2.namedWindow("result", cv2.WINDOW_NORMAL)
         # cv2.imshow("result", result)
         if isOutput:
