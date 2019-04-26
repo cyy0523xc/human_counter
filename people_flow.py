0ba7bed814db0f4e70926610e32d7431c5df3a2f
alex <cyy0523xc@gmail.com>
2019-04-26 18:50:28 +0800

    *

--- people_flow.py
+++ people_flow.py
@@ -93,7 +93,7 @@ class YOLO(object):
                 score_threshold=self.score, iou_threshold=self.iou)
         return boxes, scores, classes
 
-    def check_in_box(point, box):
+    def check_in_box(self, point, box):
         x, y = point
         (x1, y1), (x2, y2) = box
         return x1 < x < x2 and y1 < y < y2
